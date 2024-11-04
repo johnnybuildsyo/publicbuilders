846a0f7800ef2ec489a2b88fa1548c2d43aa2c2a
johnnybuildsyo <johnnybuilds@gmx.com>
2024-11-03 22:35:43 -0600

    Update header import and component implementation

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next"
 import localFont from "next/font/local"
 import { Rethink_Sans } from "next/font/google"
 import "./globals.css"
-import Header from "@/components/layout/home"
+import Header from "@/components/layout/header"
 
 const displayFont = Rethink_Sans({
   subsets: ["latin"],
--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,16 +1,34 @@
-'use client'
+import Link from "next/link"
+import { Blocks } from "lucide-react"
 
-import * as React from 'react'
-const NextThemesProvider = dynamic(
-	() => import('next-themes').then((e) => e.ThemeProvider),
-	{
-		ssr: false,
-	}
-)
-
-import { type ThemeProviderProps } from 'next-themes/dist/types'
-import dynamic from 'next/dynamic'
-
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-	return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+export default function Header() {
+  return (
+    <header className="border-b">
+      <div className="container mx-auto px-4 py-4 flex justify-between items-center">
+        <Link href="/" className="text-2xl font-bold">
+          <Blocks className="inline-block mr-2 text-fuchsia-600 relative -top-0.5 scale-110" />
+          Public Builders
+        </Link>
+        <nav>
+          <ul className="flex space-x-8">
+            <li>
+              <Link href="#directory" className="transition-all ease-in-out duration-500 hover:border-b border-foreground/30 border-dotted">
+                Directory
+              </Link>
+            </li>
+            <li>
+              <Link href="/join" className="transition-all ease-in-out duration-500 hover:border-b border-foreground/30 border-dotted">
+                Join
+              </Link>
+            </li>
+            <li>
+              <Link href="/about" className="transition-all ease-in-out duration-500 hover:border-b border-foreground/30 border-dotted">
+                About
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  )
 }
