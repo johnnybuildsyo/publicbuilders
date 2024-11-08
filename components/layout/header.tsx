1dc2a42af792129460411553be5a4a22b82cf691
johnnybuildsyo <johnnybuilds@gmx.com>
2024-11-08 17:10:23 -0600

    add github link to header

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link"
 import { Blocks } from "lucide-react"
+import { GitHubLogoIcon } from "@radix-ui/react-icons"
 
 export default function Header() {
   return (
@@ -12,18 +13,23 @@ export default function Header() {
         <nav>
           <ul className="flex space-x-8">
             <li>
-              <Link href="/" className="transition-all ease-in-out duration-500 hover:border-b border-foreground/30 border-dotted">
+              <Link href="/" className="transition-all ease-in-out duration-500 hover:border-b border-foreground/50 border-dotted">
                 Directory
               </Link>
             </li>
             <li>
-              <Link href="/join" className="transition-all ease-in-out duration-500 hover:border-b border-foreground/30 border-dotted">
+              <Link href="/join" className="transition-all ease-in-out duration-500 hover:border-b border-foreground/50 border-dotted">
                 Join
               </Link>
             </li>
             <li>
-              <Link href="/about" className="transition-all ease-in-out duration-500 hover:border-b border-foreground/30 border-dotted">
+              <Link href="/about" className="transition-all ease-in-out duration-500 hover:border-b border-foreground/50 border-dotted">
                 About
+              </Link>
+            </li>
+            <li>
+              <Link href="https://github.com/johnnybuildsyo/publicbuilders" className="transition-all ease-in-out duration-500 opacity-50 hover:opacity-70">
+                <GitHubLogoIcon className="w-6 h-6" />
               </Link>
             </li>
           </ul>
